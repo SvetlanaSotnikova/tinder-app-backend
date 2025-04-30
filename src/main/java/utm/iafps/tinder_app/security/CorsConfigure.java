905d9161ffc0281628571bfdaca00ebad3e34e14
Svetik <sotnikovas337@gmail.com>
2025-04-30 13:18:12 +0300

    upd cors

--- src/main/java/utm/iafps/tinder_app/security/CorsConfigure.java
+++ src/main/java/utm/iafps/tinder_app/security/CorsConfigure.java
@@ -14,7 +14,7 @@ public class CorsConfigure {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/**") // для фронта
+                        .allowedOrigins("http://localhost:5173") // для фронта
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
