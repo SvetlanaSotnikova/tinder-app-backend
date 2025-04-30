b1eed35986b581c0c1781f7963ac81eac6105b50
Svetik <sotnikovas337@gmail.com>
2025-04-30 23:21:23 +0300

    apd like controller

--- src/main/java/utm/iafps/tinder_app/controllers/LikeController.java
+++ src/main/java/utm/iafps/tinder_app/controllers/LikeController.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utm.iafps.tinder_app.dto.LikeRequest;
+import utm.iafps.tinder_app.models.Like;
 import utm.iafps.tinder_app.services.LikeService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/likes")
@@ -17,5 +20,10 @@ public class LikeController {
     public ResponseEntity<String> likeUser(@RequestBody @Valid LikeRequest likeRequest) {
         likeService.likeUser(likeRequest.getUsername(), likeRequest.getLikedUsername());
         return ResponseEntity.ok("Successfully liked user ");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Like>> getAllLikes(@RequestParam String username) {
+        return ResponseEntity.ok(likeService.getLikes(username));
     }
 }
